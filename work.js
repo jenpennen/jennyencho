3d00c0a2e406dc66a72105c4ccd51855d05d0029
jenpennen <jennycho35@gmail.com>
2024-10-09 20:44:15 -0700

    built out the theme toggle

--- work.js
+++ work.js
@@ -32,6 +32,29 @@ function reveal() {
     }
   });
 }
+const checkbox = document.getElementById("checkbox");
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.body.classList.add(currentTheme);
+
+  // Ensure the checkbox state matches the theme
+  if (currentTheme === "dark") {
+    checkbox.checked = true;
+  }
+}
+checkbox.addEventListener("change", () => {
+  if (checkbox.checked) {
+    // Add dark mode and save the preference
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    // Add light mode and save the preference
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    localStorage.setItem("theme", "light");
+  }
+});
 
 // SMOOTH SCROLLING
 const ease = (t, b, c, d) => {
